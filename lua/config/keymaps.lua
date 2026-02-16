666f8a4beead8fe43b7b4bcd526f9ebba9e65f65
vincent <arcobi98@gmail.com>
2026-02-17 02:17:10 +0900

    fix autocomplete

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,28 @@ map("n", "<leader>mq", "<cmd>CMakeClose<cr>", { desc = "CMake Close" })
 
 -- Automation: Build AND Run with one key (e.g., <F5>)
 map("n", "<F5>", "<cmd>CMakeBuild<cr><cmd>CMakeRun<cr>", { desc = "CMake Build and Run" })
+
+vim.keymap.set("n", "<left>", "<nop>")
+vim.keymap.set("n", "<right>", "<nop>")
+vim.keymap.set("n", "<up>", "<nop>")
+vim.keymap.set("n", "<down>", "<nop>")
+vim.keymap.set("i", "<left>", "<nop>")
+vim.keymap.set("i", "<right>", "<nop>")
+vim.keymap.set("i", "<up>", "<nop>")
+vim.keymap.set("i", "<down>", "<nop>")
+
+-- Jump to next closing delimiter in Insert Mode
+vim.keymap.set("i", "<C-l>", function()
+  local line = vim.api.nvim_get_current_line()
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  -- Find the next ), ], }, ", or ' after the cursor
+  local next_delimiter = line:find("[%)]%}%]\"']", col + 1)
+
+  if next_delimiter then
+    vim.api.nvim_win_set_cursor(0, { vim.api.nvim_win_get_cursor(0)[1], next_delimiter })
+  else
+    -- Fallback: just move right if no delimiter is found, or do nothing
+    local char_at_cursor = line:sub(col + 1, col + 1)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Right>", true, false, true), "n", true)
+  end
+end, { desc = "Jump to next closing delimiter" })
