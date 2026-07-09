d12cbcf6fb2181924dbdf3ef9458016aa63fb80d
arcobi <arcobi98@gmail.com>
2026-07-09 18:18:16 +0900

    removed autoformat

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -24,5 +24,19 @@ vim.api.nvim_create_autocmd("VimEnter", {
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
-  callback = hide_tabline,
+  callback = function()
+    hide_tabline()
+    vim.g.autoformat = false
+  end,
+})
+
+local function disable_auto_line_formatting()
+  vim.b.autoformat = false
+  vim.opt_local.textwidth = 0
+  vim.opt_local.formatoptions:remove({ "t", "c", "r", "o" })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = disable_auto_line_formatting,
 })
